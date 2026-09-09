643d4ccd7a6273c084526a32132be21f73125eab
sSamyel <samyel333@gmail.com>
2026-09-09 23:21:24 +0300

    Eight version web-app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,24 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    base: '/',  // ← добавь эту строку (явно указываем корень)
     server: {
+        host: true,
         port: 3000,
+        watch: {
+            usePolling: true
+        },
         proxy: {
             '/api': {
-                target: 'http://localhost:8080',
+                target: 'http://backend:8080',
                 changeOrigin: true,
+                secure: false
             },
-        },
-    },
+            '/auth': {
+                target: 'http://backend:8080',
+                changeOrigin: true,
+                secure: false
+            }
+        }
+    }
 });
